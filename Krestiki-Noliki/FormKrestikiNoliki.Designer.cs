d64da91ac852fd1922ae407e49f42d07afef43c8
sheron.s <sheron.s@mail.ru>
2017-05-11 19:59:18 +0300

    Создали форму и первый интерфейс

---
+++ Krestiki-Noliki/FormKrestikiNoliki.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Krestiki_Noliki
+{
+    partial class FormKrestikiNoliki
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormKrestikiNoliki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 433);
+            this.Name = "FormKrestikiNoliki";
+            this.Text = "Крестики-нолики";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
